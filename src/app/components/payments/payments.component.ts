daa1ba7fee079e0dcdfbcd74decd23b211c954f2
JoseBuenano <jbuenano@horizon-ts.com>
2018-07-19 11:24:56 -0500

    Cards

--- src/app/components/payments/payments.component.ts
+++ src/app/components/payments/payments.component.ts
@@ -3,6 +3,9 @@ import { HorizonButton } from '../../interfaces/horizon-button.interface';
 import { FormBuilder } from '@angular/forms';
 import { PaymentsService } from '../../services/payments.service';
 import { Payment } from '../../models/payments';
+import { Alert } from '../../models/alert';
+import { NotifierService } from '../../services/notifier.service';
+import { ALERT_TYPES } from '../../config/alert-types';
 
 
 declare var $: any;
@@ -19,19 +22,17 @@ export class PaymentsComponent implements OnInit {
   private _CLOSE = 1;
   private self: any;
   public matButtons: HorizonButton[];
+  private alertData: Alert;
+  
 
   constructor(
     private formBuilder: FormBuilder,
-    private _payments: PaymentsService
+    private _payments: PaymentsService,
+    private _notifierService: NotifierService,
   ) { 
     this.closeModal = new EventEmitter<boolean>();
     this.matButtons = [
-      {
-        parentContentType: 0,
-        action: this._SUBMIT,
-        icon: "check"
-      },
-      {
+       {
         parentContentType: 0,
         action: this._CLOSE,
         icon: "close"
@@ -44,18 +45,33 @@ export class PaymentsComponent implements OnInit {
     $("#hidden-btn").on(("click"), (event) => { }); //NO TOCAR!
   }
 
-  //Envio para los Pagos
-  envioPagos () {
+  //Envio para los Pagos PayPal
+  envioPagosPayPal () {
+    console.log(this.pagos);
     this._payments.postPagosClient(this.pagos).then((data) => {
       document.location.href = data.data.data;
-      //LLamar al get
+      /*LLamar al get con alert
+      this.alertData = new Alert({ title: 'Proceso Correcto', message: data.data, type: ALERT_TYPES.success });
+      this.setAlert();*/
     });
+  }
+   //Envio para los Pagos
+   envioPagosCard () {
+    this._payments.postPagosCards(this.pagos).then((data) => {
+      document.location.href = data.data.data;
+      /*LLamar al get con alert
+      this.alertData = new Alert({ title: 'Proceso Correcto', message: data.data, type: ALERT_TYPES.success });
+      this.setAlert();*/
+    });
+  }
+  setAlert() {
+    this._notifierService.sendAlert(this.alertData);
   }
   getButtonAction(actionEvent: number) {
     switch (actionEvent) {
-      case this._SUBMIT:
+      /*case this._SUBMIT:
         this.envioPagos();
-        break;
+        break;*/
       case this._CLOSE:
         this.closeModal.emit(true);
         break;
