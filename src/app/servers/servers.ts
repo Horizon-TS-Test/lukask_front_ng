abf039a8d4aa7cdfedd1e46a4bb07535602f0e13
fredrom <wfrediromans@outlook.com>
2018-08-15 10:56:00 -0500

    -CHANGING DEVELOP URL'S

--- src/app/servers/servers.ts
+++ src/app/servers/servers.ts
@@ -1,5 +1,5 @@
 export const SERV_IP = {
-    middleware: 'https://www.lukaksarticles.com',
-    push: 'https://push.lukaksarticles.com',
-    kurentoClient: 'wss://kms.lukaksarticles.com'
+    middleware: 'http://192.168.1.62:3001',
+    push: 'http://192.168.1.14:3200',
+    kurentoClient: 'wss://192.168.1.13:8888'
 }
