15dae406694c76628eba6060f91c22633aa3ba62
dennysmoyon <dmoyon@horizon-ts.com>
2018-09-10 13:00:32 -0500

    cambios para proceso de grabacion de video.

--- src/app/servers/servers.ts
+++ src/app/servers/servers.ts
@@ -1,5 +1,7 @@
 export const SERV_IP = {
     middleware: 'http://192.168.1.62:3001',
     push: 'http://192.168.1.14:3200',
-    kurentoClient: 'wss://192.168.1.13:8080'
+    kurentoClient: 'ws://192.168.1.59:8080',
+    kurentoServer: 'wss://192.168.1.59:8443',
+    fileRepository: 'file:///temp/pruebasKMS.webm'
 }
--- src/app/services/webrtc-socket.service.ts
+++ src/app/services/webrtc-socket.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import * as kurentoUtils from 'kurento-utils';
+import * as kurentoClientLibs from 'kurento-client';
 import { REST_SERV } from '../rest-url/rest-servers';
 import { QuejaService } from './queja.service';
+import { SERV_IP } from "../servers/servers";
+declare var MediaRecorder:any;
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +13,16 @@ import { QuejaService } from './queja.service';
 //HREF ABOUT WEBSOCKET: https://developer.mozilla.org/es/docs/Web/API/WebSocket
 export class WebrtcSocketService {
   private webRtcPeer: any;
+  /*private webRtc:any;
+  private recordRtc:any;
+  private pipeline:any;*/
+  private stream:any;
+  private recordedBlobs:any[];
+  private mediaRecorder:any;
   private _videoData: any;
   private userId: any;
   private pubId: any;
   private isPresenter: any;
-
   public kurentoWs: any;
   public video: any;
 
@@ -22,6 +30,7 @@ export class WebrtcSocketService {
     private _quejaService: QuejaService
   ) {
     this.isPresenter = false;
+    this.recordedBlobs  = [];
   }
 
   /**
@@ -106,9 +115,7 @@ export class WebrtcSocketService {
         mediaConstraints: constraints,
         onicecandidate: (candidate) => { this.onIceCandidate(candidate) }
       }
-
       console.log("options....", options);
-
       this.webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendonly(options, (error) => {
 
         if (error) {
@@ -118,6 +125,13 @@ export class WebrtcSocketService {
 
         this.isPresenter = true;
       });
+
+      //Proceso de incio de grabación.
+      this.dataRecord(constraints);
+
+      //Proceso de grabación.
+      this.startRecording();
+
     }
   }
 
@@ -128,13 +142,68 @@ export class WebrtcSocketService {
 
     if (error) {
       console.log("error al ofertar", error)
+      return error;
     }
+
     let message = {
       keyWord: 'presenter',
       sdpOffer: offerSdp,
       userId: this.userId
     };
     this.sendMessage(message);
+
+    /*kurentoClientLibs(SERV_IP.kurentoServer, (error, client) =>{
+      if(error){
+        console.log("Error al conectar", error);
+        return error;
+      }
+
+      client.create('MediaPipeline', (error, pipeline) =>{
+        if(error){
+          console.log("Error al crear el pipeline", error);
+          return error;
+        }
+        
+        let elements = [
+          {type: 'RecorderEndPoint', params : {uri : SERV_IP.fileRepository}},
+          {type: 'WebRtcEndpoint', params: {}}
+        ];
+
+        this.pipeline = pipeline;
+        this.pipeline.create(elements, (error, elements) => {
+          if(error){
+            console.log("error al crear el pipeline", error);
+            return error;
+          }
+
+          this.recordRtc = elements[0];
+          this.webRtc   = elements[1];
+          //this.setIceCandidateCallbacks();
+
+          this.webRtc.processOffer(offerSdp, (error, answer) =>{
+            if(error){
+              console.log("error al procesar la oferta", error);
+              return error;
+            }
+            this.webRtcPeer.processAnswer(answer);
+          });
+
+          client.connect(this.webRtc, this.recordRtc, (error) =>{
+            if(error){
+              console.log("error al conectar al recorder", error);
+              return error;
+            }
+
+            this.recordRtc.record((error) =>{
+              if(error) {
+                console.log("Error en el proceso de grabacion", error);
+                return error;
+              }
+            });
+          });
+        });
+      });
+    }); */
   }
 
   /**
@@ -214,7 +283,12 @@ export class WebrtcSocketService {
       }
       console.log("messege", messege);
       this.sendMessage(messege);
+      //this.recordRtc.stop();
+      //this.pipeline.release();
       this.dispose();
+
+      //Detener la transmición.
+      this.stopRecorder();
     }
   }
 
@@ -264,4 +338,100 @@ export class WebrtcSocketService {
   private closeWebSocketConn() {
     this.kurentoWs.close();
   }
+
+  /*setIceCandidateCallbacks(){
+    this.webRtcPeer.on('icecandidate', (candidate) =>{
+      console.log("Local candidate:",candidate);
+      candidate = kurentoClientLibs.getComplexType('IceCandidate')(candidate);
+      webRtcEp.addIceCandidate(candidate, onerror);
+    });
+
+    webRtcEp.on('OnIceCandidate', (event) => {
+      var candidate = event.candidate;
+      console.log("Remote candidate:",candidate);
+      webRtcPeer.addIceCandidate(candidate, onerror);
+    });
+  }*/
+
+
+  dataRecord(constraints){
+    try{
+      this.stream = navigator.mediaDevices.getUserMedia(constraints);
+    } catch(e){
+      console.log('navigator.getUserMedia error', e);
+    }
+  }
+  
+  /**
+   * Inicio del proceso de grabación.
+   */
+  startRecording(){
+    
+    this.recordedBlobs = [];
+
+    //Opciones de datos.
+    let options = {
+      mimeType : 'video/webm;codecs=vp9'
+    }
+
+    //Proceso de validacion de soporte de codec's para el navegador.
+    if(!MediaRecorder.isTypeSupported(options.mimeType)){
+      
+      console.error(`${options.mimeType} no es soportado`);
+      options = { mimeType : 'video/webm;codecs=vp8'};
+      if(!MediaRecorder.isTypeSupported(options.mimeType)){
+        
+        console.error(`${options.mimeType} no es soportado`);
+        if(!MediaRecorder.isTypeSupported(options.mimeType)){
+          
+          console.log(`${options.mimeType} no es soportado`);
+          options = {mimeType : ''};
+        }
+      }
+    }
+
+    //Objeto que contiene la grabación.
+    try{
+      this.mediaRecorder = new MediaRecorder(this.stream, options);
+    }catch(error){
+      console.error('Error en el proceso de grabación:', error);
+      return error;
+    }
+    console.info('Proceso de grabacion exitoso', this.mediaRecorder, ' Con opciones de grabación:', options);
+    
+    //Escuchamos el evento de detenido de la grabación
+    this.mediaRecorder.onstop = (event) =>{
+      console.info("La grabación de ha detenido", event);
+    }
+
+    //proceso de almacenamiento de blob´s de la transmición
+    this.mediaRecorder.ondataavailable = this.handleDataAvailable;
+
+    //Escuchamos el evento de inicio de la grabación.
+    this.mediaRecorder.start(10);
+
+    console.info('Media recorder iniciado', this.mediaRecorder);
+  }
+
+  /**
+   * Proceso de alamacenamiento de blod del video
+   * @param event 
+   */
+  handleDataAvailable(event){
+    if(event.data && event.data.size > 0){
+      this.recordedBlobs.push(event.data);
+    }
+  }
+
+  stopRecorder(){
+    this.mediaRecorder.stop();
+    console.log('Recorder Blobs:', this.recordedBlobs);
+    let bufferContainer = new Blob(this.recordedBlobs, {type: 'video/webm'});
+    console.log("cadena de video: ", bufferContainer);
+    let urlDataVideo = window.URL.createObjectURL(bufferContainer);
+    console.log("URL para descarga devideo", urlDataVideo);
+  }
+
+
 }
+
