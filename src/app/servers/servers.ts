9b019cfdbdebebc8293abb473de42f7a8c4440cf
fredrom <wfrediromans@outlook.com>
2018-09-28 09:02:35 -0500

    -FIXING SERVER NAME ERROR

--- src/app/servers/servers.ts
+++ src/app/servers/servers.ts
@@ -1,5 +1,5 @@
 export const SERV_IP = {
     middleware: 'https://www.lukaksarticles.com',
-    push: 'https://www.push.lukaksarticles.com',
+    push: 'https://push.lukaksarticles.com',
     kurentoClient: 'wss://kms.lukaksarticles.com'
 }
