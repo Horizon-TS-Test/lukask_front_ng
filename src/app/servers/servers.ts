d2a3bc94b58f7b139ee459bc31255ed71f21f167
fredrom <wfrediromans@outlook.com>
2018-11-09 18:37:06 -0500

    -DELETING UNNECESSARY SERVER DOMAIN

--- src/app/servers/servers.ts
+++ src/app/servers/servers.ts
@@ -1,7 +1,6 @@
 export const SERV_IP = {
-    middleware: 'http://192.168.1.62:3001',
+    middleware: 'http://192.168.1.37:3001',
     push: 'http://192.168.1.14:3200',
     kurentoClient: 'ws://192.168.1.11:8080',
-    middleware_kms: 'http://192.168.1.11:8080',
-    fileRepository: 'file:///temp/pruebasKMS.webm'
+    middleware_kms: 'http://192.168.1.11:8080'
 }
