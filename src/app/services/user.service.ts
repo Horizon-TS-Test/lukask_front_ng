cb89f785fe548ab65b47aa7a001b366519bf4026
fredrom <wfrediromans@outlook.com>
2018-11-13 18:50:22 -0500

    -IMPLEMENTING A GET LOCATION (PROVINCE, CANTON AND PARISH) PROCESS IN AN INDEPENDENT SERVICE CLASS

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,11 +5,9 @@ import { REST_SERV } from '../rest-url/rest-servers';
 import { CrytoGen } from '../tools/crypto-gen';
 import { Http, Headers, Response } from '@angular/http';
 import { BackSyncService } from './back-sync.service';
-import { Province } from '../models/province';
-import { Canton } from '../models/canton';
-import { Parroquia } from '../models/parroquia';
 import { DateManager } from '../tools/date-manager';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { LocationService } from './location.service';
 
 declare var writeData: any;
 declare var readAllData: any;
@@ -22,9 +20,6 @@ export class UserService {
   private updateUserSub = new BehaviorSubject<boolean>(null)
   updateUser$: Observable<boolean> = this.updateUserSub.asObservable();
 
-  private isFetchedProvince: boolean;
-  private isFetchedCanton: boolean;
-  private isFetchedParroquia: boolean;
   private isPatchedUser: boolean;
 
   public userProfile: User;
@@ -34,11 +29,9 @@ export class UserService {
   constructor(
     private _http: Http,
     private _backSyncService: BackSyncService,
+    private _locationService: LocationService
   ) {
     this.pageLimit = 5;
-    this.isFetchedProvince = false;
-    this.isFetchedCanton = false;
-    this.isFetchedParroquia = false;
     this.isPatchedUser = false;
     this.onStreaming = false;
   }
@@ -222,7 +215,7 @@ export class UserService {
   /**
     * MÉTODO PARA TOMAR LOS DATOS QUE BIENEN POR POST PARA REGISTRO
     */
-  mergeFormData(user: User) {
+  private mergeFormData(user: User) {
     let formData = new FormData();
 
     formData.append('id', user.id);
@@ -250,7 +243,7 @@ export class UserService {
    * MÉTODO PARA ENVIAR MEDIANTE POST LOS DATOS DEL PERFIL
    * @param userFormData 
    */
-  postUserClient(userFormData: FormData) {
+  private postUserClient(userFormData: FormData) {
     return new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function () {
@@ -283,22 +276,13 @@ export class UserService {
    * MÉTODO PARA EXTRAER LOS DATOS DE USUARIO DE UN JSON STRING Y GUARDARLO EN UN OBJETO DE TIPO MODELO USER
    * @param jsonUser ES EL JSON STRING QUE CONTIENE LOS DATOS DEL USUARIO
    */
-  extractUserJson(jsonUser: any) {
+  public extractUserJson(jsonUser: any) {
     let user: User;
     jsonUser.profile_path = jsonUser.profile_path.indexOf("http") !== -1 || jsonUser.profile_path.indexOf("https") !== -1 ? jsonUser.profile_path : REST_SERV.mediaBack + jsonUser.profile_path;
     user = new User(jsonUser.email, '', jsonUser.profile_path, jsonUser.is_active, null, null, jsonUser.id);
     user.person = new Person(jsonUser.person.id_person, jsonUser.person.age, jsonUser.person.identification_card, jsonUser.person.name, jsonUser.person.last_name, jsonUser.person.telephone, jsonUser.person.address, jsonUser.person.active, jsonUser.person.birthdate, jsonUser.person.cell_phone, null, DateManager.convertStringToDate(jsonUser.person.birthdate));
 
-    //PROVISIONAL
-    /*user.person.parroquia.id_parroquia = jsonUser.person.location.parish.id;
-    user.person.parroquia.name = jsonUser.person.location.parish.description;
-
-    user.person.parroquia.canton.id_canton = jsonUser.person.location.canton.id;
-    user.person.parroquia.canton.name = jsonUser.person.location.canton.description;
-
-    user.person.parroquia.canton.province.id_province = jsonUser.person.location.province.id;
-    user.person.parroquia.canton.province.name = jsonUser.person.location.province.description;*/
-    ////
+    this._locationService.extractLocationJson(jsonUser.person.location, user.person);
 
     return user;
   }
@@ -306,7 +290,7 @@ export class UserService {
   /**
    * MÉTODO PARA OBTENER LA INFORMACIÓN DEL PERFIL DE USUARIO Y DESENCRIPTARLO
    */
-  getStoredUserData() {
+  private getStoredUserData() {
     let storedData = localStorage.getItem('user_data');
     let user;
     if (storedData) {
@@ -320,14 +304,14 @@ export class UserService {
   /**
    * MÉTODO PARA DETERMINAR SI UN USUARIO ESTÁ O NO LOGGEADO
    */
-  isLoggedIn() {
+  public isLoggedIn() {
     return localStorage.getItem('user_key') !== null;
   }
 
   /**
    * MÉTODO PARA OBTENER EL ID DE UN USUARIO PARA PODER REALIZAR LOS REQUEST AL SERVIDOR
    */
-  getUserKey() {
+  public getUserKey() {
     const user_key = localStorage.getItem("user_key") ? localStorage.getItem("user_key") : '';
     return user_key;
   }
@@ -370,7 +354,7 @@ export class UserService {
    * MÉTODO PARA ENVIAR MEDIANTE POST LOS DATOS DEL PERFIL
    * @param userFormData 
    */
-  patchUserClient(userFormData: FormData) {
+  private patchUserClient(userFormData: FormData) {
     return new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function () {
@@ -410,218 +394,5 @@ export class UserService {
 
         return true;
       });
-  }
-
-  /**
-   * MÉTODO PARA OBTENER LAS PROVINCIAS SEA DE LA WEB O DE LA CACHÉ
-   * */
-  public getProvinceList() {
-    return this.getProvinceWeb().then((webProvince: Province[]) => {
-      if (!this.isFetchedProvince) {
-        return this.getProvinceCache().then((cacheProvince: Province[]) => {
-          return cacheProvince;
-        });
-      }
-      else {
-        this.isFetchedProvince = false;
-      }
-
-      return webProvince;
-    }).catch((error: Response) => {
-      if (error.json().code == 401) {
-        localStorage.clear();
-      }
-      console.log(error.json());
-    });
-  }
-
-  /**
-   * MÉTODO PARA CARGAR LAS PROVINCIAS DESDE LA WEB
-   */
-  private getProvinceWeb() {
-    const qTheaders = new Headers({ 'Content-Type': 'application/json' });
-
-    return this._http.get(REST_SERV.provinceUrl, { headers: qTheaders, withCredentials: true }).toPromise()
-      .then((response: Response) => {
-        const provinces = response.json().data;
-        let transformedProvinces: Province[] = [];
-        for (let prov of provinces) {
-          transformedProvinces.push(new Province(prov.id_province, prov.description_province));
-        }
-        this.isFetchedProvince = true;
-        console.log("[LUKASK USER SERVICE] - PROVINCIAS FROM WEB", transformedProvinces);
-        return transformedProvinces;
-      })
-      .catch((error: Response) => {
-        if (error.json().code == 401) {
-          localStorage.clear();
-        }
-        console.log(error.json());
-      });
-  }
-
-  /**
-   * MÉTODO PARA CARGAR LAS PROVINCIAS DESDE LA WEB
-   */
-  private getProvinceCache() {
-    if ('indexedDB' in window) {
-      return readAllData('province')
-        .then((provinces) => {
-          let transformedProvinces: Province[] = [];
-          for (let prov of provinces) {
-            transformedProvinces.push(new Province(prov.id_province, prov.description_province));
-          }
-
-          console.log("[LUKASK USER SERVICE] - PROVINCIAS FROM CACHE", transformedProvinces);
-          return transformedProvinces;
-        });
-    }
-    return new Promise((resolve, reject) => {
-      reject(null);
-    });
-  }
-
-  /**
-   * MÉTODO PARA OBTENER LOS CANTONES DE UNA PROVINCIA SEA DE LA WEB O DE LA CACHÉ
-   * */
-  public getCantonList(id_provincia: string) {
-    return this.getCantonWeb(id_provincia).then((webCanton: Canton[]) => {
-      if (!this.isFetchedCanton) {
-        return this.getCantonCache(id_provincia).then((cacheCanton: Canton[]) => {
-          return cacheCanton;
-        });
-      }
-      else {
-        this.isFetchedCanton = false;
-      }
-
-      return webCanton;
-    }).catch((error: Response) => {
-      if (error.json().code == 401) {
-        localStorage.clear();
-      }
-      console.log(error.json());
-    });
-  }
-
-  /**
-   * MÉTODO PARA CARGAR LOS CANTONES DE UNA PROVINCIA ESPECÍFICA DESDE LA WEB
-   * @param id_provincia 
-   */
-  private getCantonWeb(id_provincia: string) {
-    const qTheaders = new Headers({ 'Content-Type': 'application/json' });
-    return this._http.get(REST_SERV.cantonUrl + "/?province_id=" + id_provincia, { headers: qTheaders, withCredentials: true }).toPromise()
-      .then((response: Response) => {
-        const cantones = response.json().data;
-        let transformedCantones: Canton[] = [];
-        for (let canton of cantones) {
-          transformedCantones.push(new Canton(canton.id_canton, canton.description_canton));
-        }
-        this.isFetchedCanton = true;
-
-        console.log("[LUKASK USER SERVICE] - CANTONES FROM WEB", transformedCantones);
-        return transformedCantones;
-      })
-      .catch((error: Response) => {
-        if (error.json().code == 401) {
-          localStorage.clear();
-        }
-        console.log(error.json());
-      });
-  }
-
-  /**
-   * MÉTODO PARA CARGAR LOS CANTONES DE UNA PROVINCIA ESPECÍFICA DESDE LA CACHÉ
-   * @param id_provincia 
-   */
-  private getCantonCache(id_provincia: string) {
-    if ('indexedDB' in window) {
-      return readAllData('canton')
-        .then((cantones) => {
-          let transformedCantones: Canton[] = [];
-          for (let canton of cantones) {
-            if (canton.province == id_provincia) {
-              transformedCantones.push(new Canton(canton.id_canton, canton.description_canton));
-            }
-          }
-          console.log("[LUKASK USER SERVICE] - CANTONES FROM CACHE", transformedCantones);
-          return transformedCantones;
-        });
-    }
-    return new Promise((resolve, reject) => {
-      reject(null);
-    });
-  }
-
-  /**
-   * MÉTODO PARA OBTENER LAS PARROQUIAS SEA DE LA WEB O DE LA CACHÉ
-   * */
-  public getParroquiaList(canton_id: any) {
-    return this.getParroquiaWeb(canton_id).then((webParroquia: Parroquia[]) => {
-      if (!this.isFetchedParroquia) {
-        return this.getParroquiaCache(canton_id).then((cacheParroquia: Canton[]) => {
-          return cacheParroquia;
-        });
-      }
-      else {
-        this.isFetchedCanton = false;
-      }
-
-      return webParroquia;
-    }).catch((error: Response) => {
-      if (error.json().code == 401) {
-        localStorage.clear();
-      }
-      console.log(error.json());
-    });
-  }
-
-  /**
-   * MÉTODO PARA CARGAR LAS PARROQUIAS DE UN CANTÓN ESPECÍFICO DESDE LA WEB
-   * @param canton_id 
-   */
-  private getParroquiaWeb(canton_id: string) {
-    const qTheaders = new Headers({ 'Content-Type': 'application/json' });
-    return this._http.get(REST_SERV.parroquiaUrl + "/?canton_id=" + canton_id, { headers: qTheaders, withCredentials: true }).toPromise()
-      .then((response: Response) => {
-        const parroquias = response.json().data;
-        let transformedParroquias: Parroquia[] = [];
-        for (let parroq of parroquias) {
-          transformedParroquias.push(new Parroquia(parroq.id_parish, parroq.description_parish));
-        }
-        this.isFetchedParroquia = true;
-
-        console.log("[LUKASK USER SERVICE] - PARROQUIAS FROM WEB", transformedParroquias);
-        return transformedParroquias;
-      })
-      .catch((error: Response) => {
-        if (error.json().code == 401) {
-          localStorage.clear();
-        }
-        console.log(error.json());
-      });
-  }
-
-  /**
-   * MÉTODO PARA CARGAR LAS PARROQUIAS DE UN CANTÓN ESPECÍFICO DESDE LA CACHÉ
-   * @param canton_id 
-   */
-  private getParroquiaCache(canton_id: string) {
-    if ('indexedDB' in window) {
-      return readAllData('parroquia')
-        .then((parroquias) => {
-          let transformedParroquias: Parroquia[] = [];
-          for (let parroq of parroquias) {
-            if (parroq.canton == canton_id) {
-              transformedParroquias.push(new Parroquia(parroq.id_parish, parroq.description_parish));
-            }
-          }
-          console.log("[LUKASK USER SERVICE] - PARROQUIAS FROM CACHE", transformedParroquias);
-          return transformedParroquias;
-        });
-    }
-    return new Promise((resolve, reject) => {
-      reject(null);
-    });
   }
 }
